17f9918522a96852cde8aabcc4e0e4c8dd342a8c
Eduard Ghiuzan <ghiuzaneduard@gmail.com>
2021-04-21 14:37:08 +0300

    test hello 3

--- src/main/java/IoT/Messaging_App/App.java
+++ src/main/java/IoT/Messaging_App/App.java
@@ -5,6 +5,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("Hello!");
-        System.out.println("Hello_3!");
+        System.out.println("Hello_3!!");
     }
 }
