71e1db1cfec5ff1d914de934ae4006ef2549f26d
Topoliceanu Calin <topoliceanuc@gmail.com>
2021-04-21 15:00:02 +0300

    test

--- src/main/java/IoT/Messaging_App/App.java
+++ src/main/java/IoT/Messaging_App/App.java
@@ -5,8 +5,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("Hello!");
+        System.out.println("Hello_3!!");
         System.out.println("Hello2!");
+<<<<<<< HEAD
         System.out.println("Salut");
        
+=======
+        System.out.println("Hello4!");
+>>>>>>> branch 'master' of https://github.com/eddie0101/IoT-App-Project.git
     }
 }
