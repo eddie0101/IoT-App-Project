10ba6817dd72fc208e59ca8061b7ba24088e3cb2
Eduard <Eduard@DESKTOP-1NA3O9F>
2021-05-19 14:05:31 +0300

    Basic communication between the app and mosquitto

--- src/main/java/IoT/Messaging_App/App.java
+++ src/main/java/IoT/Messaging_App/App.java
@@ -1,14 +1,79 @@
 package IoT.Messaging_App;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println("Hello!");
-        System.out.println("Hello_3!!");
-        System.out.println("Hello2!");
-        System.out.println("Hello4!");
-        System.out.println("Salut");
-        System.out.println("Hello4!");
+	
+	public static void main(String[] args) throws InterruptedException {
+
+        String topic        = "MQTT Examples";
+        String content      = "Message from MqttPublishSample";
+        int qos             = 2;
+        String broker       = "tcp://127.0.0.1:1883";
+        String clientId     = "JavaSample";
+        MemoryPersistence persistence = new MemoryPersistence();
+
+        try {
+            @SuppressWarnings("resource")
+			MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            System.out.println("Connecting to broker: "+broker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+            System.out.println("Publishing message: "+content);
+            MqttMessage message = new MqttMessage(content.getBytes());
+            message.setQos(qos);
+            sampleClient.publish(topic, message);
+            System.out.println("Message published");
+            sampleClient.subscribe("Topic_Test");
+            System.out.println("Subscribed to Topic_Test");
+            
+            
+            
+            //sampleClient.messageArrivedComplete(qos, qos);
+            
+            System.out.println("Subscribed to Topic_Test");
+            
+            sampleClient.setCallback(new MqttCallback() {
+				
+				@Override
+				public void messageArrived(String topic, MqttMessage message) throws Exception {
+					// TODO Auto-generated method stub
+					System.out.println(message.toString());
+				}
+				
+				@Override
+				public void deliveryComplete(IMqttDeliveryToken token) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void connectionLost(Throwable cause) {
+					// TODO Auto-generated method stub
+					
+				}
+			});
+            
+            Thread.sleep(15000);
+            sampleClient.disconnect();
+            System.out.println("Disconnected");
+            //System.exit(0);
+        } catch(MqttException me) {
+            System.out.println("reason "+me.getReasonCode());
+            System.out.println("msg "+me.getMessage());
+            System.out.println("loc "+me.getLocalizedMessage());
+            System.out.println("cause "+me.getCause());
+            System.out.println("excep "+me);
+            me.printStackTrace();
+        }
     }
 }
