3208e88b9cd66a5ec2d98378e3aea1c3917e6c9b
Eduard <Eduard@DESKTOP-1NA3O9F>
2021-05-26 17:58:59 +0300

    Dark theme GUI done

--- src/main/java/IoT/Messaging_App/GuiApp.java
+++ src/main/java/IoT/Messaging_App/GuiApp.java
@@ -18,6 +18,9 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.SwingConstants;
 import java.awt.Font;
 import javax.swing.JPasswordField;
+import java.awt.Color;
+import java.awt.SystemColor;
+import javax.swing.UIManager;
 
 
 public class GuiApp {
@@ -45,7 +48,9 @@ public class GuiApp {
 	
 	void initialize() {
 		frame = new JFrame("GUI");
+		frame.setTitle("MyIRC");
 		panel1= new JPanel();
+		panel1.setBackground(Color.DARK_GRAY);
 		
 		frame.setBounds(100, 100, 701, 525);
 		frame.setResizable(false);
@@ -59,23 +64,29 @@ public class GuiApp {
 		frame.getContentPane().add(panel1);
 		
 		JLabel lblNewLabel_1 = new JLabel("Host");
+		lblNewLabel_1.setForeground(Color.LIGHT_GRAY);
 		panel1.add(lblNewLabel_1);
 		
 		textField_Host = new JTextField();
+		textField_Host.setBackground(Color.GRAY);
 		panel1.add(textField_Host);
 		textField_Host.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("Port");
+		lblNewLabel_2.setForeground(Color.LIGHT_GRAY);
 		panel1.add(lblNewLabel_2);
 		
 		textField_Port = new JTextField();
+		textField_Port.setBackground(Color.GRAY);
 		panel1.add(textField_Port);
 		textField_Port.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("ClientID");
+		lblNewLabel.setForeground(Color.LIGHT_GRAY);
 		panel1.add(lblNewLabel);
 		
 		textField_ClientID = new JTextField();
+		textField_ClientID.setBackground(Color.GRAY);
 		panel1.add(textField_ClientID);
 		textField_ClientID.setColumns(10);
 	
@@ -83,48 +94,61 @@ public class GuiApp {
 		
 		
 		JLabel lblNewLabel_3 = new JLabel("Username");
+		lblNewLabel_3.setForeground(Color.LIGHT_GRAY);
 		panel1.add(lblNewLabel_3);
 		
 		textField_3 = new JTextField();
+		textField_3.setBackground(Color.GRAY);
 		panel1.add(textField_3);
 		textField_3.setColumns(10);
 		
 		JLabel lblNewLabel_4 = new JLabel("Password");
+		lblNewLabel_4.setForeground(Color.LIGHT_GRAY);
 		panel1.add(lblNewLabel_4);
 		
 		textField_4 = new JPasswordField();
+		textField_4.setBackground(Color.GRAY);
 		panel1.add(textField_4);
 		textField_4.setColumns(10);
 		
 		JLabel lblNewLabel_5 = new JLabel("Keep Alive");
+		lblNewLabel_5.setForeground(Color.LIGHT_GRAY);
 		panel1.add(lblNewLabel_5);
 		
 		textField_5 = new JTextField();
+		textField_5.setBackground(Color.GRAY);
 		panel1.add(textField_5);
 		textField_5.setColumns(10);
 		
 		JLabel lblNewLabel_6 = new JLabel("Last-will Topic");
+		lblNewLabel_6.setForeground(Color.LIGHT_GRAY);
 		panel1.add(lblNewLabel_6);
 		
 		textField_6 = new JTextField();
+		textField_6.setBackground(Color.GRAY);
 		panel1.add(textField_6);
 		textField_6.setColumns(10);
 		
 		JLabel lblNewLabel_7 = new JLabel("QoS");
+		lblNewLabel_7.setForeground(Color.LIGHT_GRAY);
 		
 		panel1.add(lblNewLabel_7);
 		
 		JComboBox comboBox = new JComboBox();
+		comboBox.setBackground(Color.LIGHT_GRAY);
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"0", "1", "2"}));
 		comboBox.setMaximumRowCount(3);
 		comboBox.setToolTipText("1");
 		panel1.add(comboBox);
 		
 		JCheckBox chckbxNewCheckBox = new JCheckBox("Last-Will Retain");
+		chckbxNewCheckBox.setForeground(Color.LIGHT_GRAY);
+		chckbxNewCheckBox.setBackground(Color.DARK_GRAY);
 		chckbxNewCheckBox.setHorizontalAlignment(SwingConstants.CENTER);
 		panel1.add(chckbxNewCheckBox);
 		
 		JPanel panel = new JPanel();
+		panel.setBackground(Color.DARK_GRAY);
 		panel.setBorder(BorderFactory.createEmptyBorder(30,30, 10, 30));
 		panel.setLayout(new GridLayout(0,1));
 		
@@ -138,6 +162,7 @@ public class GuiApp {
 		panel.add(lblNewLabel_8_1);
 		
 		JTextArea textArea_1 = new JTextArea();
+		textArea_1.setBackground(Color.GRAY);
 		textArea_1.setTabSize(10);
 		textArea_1.setRows(5);
 		panel.add(textArea_1);
